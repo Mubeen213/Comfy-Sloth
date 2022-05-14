80374887a43ef776f9ac1676b8a1a958907b1451
Mubeen <mdmubeen1307@gmail.com>
2022-05-14 14:32:31 +0530

    news letter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            veritatis obcaecati vel harum neque natus, consequuntur aut
+            laboriosam. Pariatur odio officia non adipisci sit et dolorum
+            consectetur recusandae vitae perspiciatis?
+          </p>
+          <form action='' className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='Enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +91,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
