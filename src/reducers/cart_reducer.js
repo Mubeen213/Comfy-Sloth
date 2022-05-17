81a911839084c7c2bcdeae1955044eb9192513d2
Mubeen <mdmubeen1307@gmail.com>
2022-05-17 12:26:12 +0530

    add to cart existing products

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -10,7 +10,21 @@ const cart_reducer = (state, action) => {
   if (action.type === ADD_TO_CART) {
     const { id, color, amount, product } = action.payload;
     const tempItems = state.cart.find((item) => item.id === id + color);
+
     if (tempItems) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+
+      return { ...state, cart: tempCart };
     } else {
       const newItem = {
         id: id + color,
