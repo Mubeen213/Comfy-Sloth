7a74b8d5b9a5b20ccc0e15295e730e7168f31b07
Mubeen <mdmubeen1307@gmail.com>
2022-05-17 12:13:43 +0530

    Add to cart and cart context

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,29 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from '../actions';
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItems = state.cart.find((item) => item.id === id + color);
+    if (tempItems) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
 
-export default cart_reducer
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+    return { ...state };
+  }
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
